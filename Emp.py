d69d833ae8e8c8c948f9c73340e232d91027b3a8
Kaustubh Kale <kaustubh.mum.dbda@gmail.com>
2024-04-18 17:32:34 +0530

    [Kaustubh Kale] Add: wage calculation based on part time hours

--- Emp.py
+++ Emp.py
@@ -1,11 +1,12 @@
 import random as rm
-attendance=rm.randint(0,1)
-if attendance==1:
-    print("employee is Present")
-else:
-    print("employee is Absent ")
-
-#uc2
+attendance=rm.randint(0,2)
 rate_per_hour=20
 total_hour=8
-print("total Payable wage is",attendance*rate_per_hour*total_hour)
+part_time_hour=4
+if attendance==1:
+    print("employee is Present",attendance*rate_per_hour*total_hour)
+elif attendance==2:
+    print("employee is present and done a part time ",attendance*rate_per_hour*(total_hour+part_time_hour))
+else:
+    print("employee is absent")
+
