d2eae480262863676775875d7c213686b03114cc
Kaustubh Kale <kaustubh.mum.dbda@gmail.com>
2024-04-18 16:51:25 +0530

    [Kaustubh Kale] Add : Script to calculate the daily wage of an employee

--- Emp.py
+++ Emp.py
@@ -4,3 +4,8 @@ if attendance==1:
     print("employee is Present")
 else:
     print("employee is Absent ")
+
+#uc2
+rate_per_hour=20
+total_hour=8
+print("total Payable wage is",attendance*rate_per_hour*total_hour)
