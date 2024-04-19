bab45bf203d45017ca51494bceaccbc090bc46db
Kaustubh Kale <kaustubh.mum.dbda@gmail.com>
2024-04-19 11:13:57 +0530

    [Kaustubh Kale] Refactor : change the code and add working hour display function

--- Emp.py
+++ Emp.py
@@ -1,5 +1,5 @@
 import random as rm
-#uc6_cal_wages_with_conditions
+#UC7_function_workinghours
 RATE_PER_HOUR=20
 TOTAL_HOUR=8
 PART_TIME_HOUR=4
@@ -17,11 +17,13 @@ def check_attendance():
             WORKING_DAY_PER_MONTH +=1
             TOTAL_WORKING_HOURS +=TOTAL_HOUR
             calculate_wage()
+            work_hours()
         case 2:
             print("Employee is present and done a partime ")
             WORKING_DAY_PER_MONTH+= 1
             TOTAL_WORKING_HOURS += (PART_TIME_HOUR + TOTAL_HOUR)
             calculate_wage_partime()
+            work_hours()
         case _ :
             print("Employee is absent")
 
@@ -34,10 +36,13 @@ def calculate_wage_partime():
 def total_monthly_wage():
     print("The total monthly wage for the employee is",TOTAL_WORKING_HOURS*RATE_PER_HOUR)
 
+def work_hours():
+    print("total work hours completed ", TOTAL_WORKING_HOURS)
 
 
 
-while WORKING_DAY_PER_MONTH <=20 or TOTAL_WORKING_HOURS <=100:
+
+while WORKING_DAY_PER_MONTH <=20 and TOTAL_WORKING_HOURS <=100:
     check_attendance()
 
 total_monthly_wage()
